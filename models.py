48fd0c9f68819ab7ec8d9335d95c563dacc2dc56
KFOLW <kfolweiler@live.com>
2017-03-28 10:47:45 -0400

    Initialize alembic migrations

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+'''
+
 from app import db 
 
 from datetime import datetime
@@ -10,27 +12,27 @@ from sqlalchemy.orm import relationship, backref
 Base = declarative_base()
 
 class Cage(Base):
-	 __tablename__ = 'cages'
+      __tablename__ = 'cages'
 
-	cage_id = Column(Integer(), primary_key=True)
-   	cage_number = Column(Integer(), index=True)
-    	genotype = Column(String(255))
-    	sex = Column(String(255))
-    	quantity = Column(Integer())
-    	dob = Column(DateTime(), default=datetime.now)
-    	injured = Column(Boolean(), default=False)
+      cage_id = Column(Integer(), primary_key=True)
+      cage_number = Column(Integer(), index=True)
+      genotype = Column(String(255))
+      sex = Column(String(255))
+      quantity = Column(Integer())
+      dob = Column(DateTime(), default=datetime.now)
+      injured = Column(Boolean(), default=False)
 
 
-    	def __init__(self, number, genotype, sex, quantity=0, dob, injured):
-        	self.cage_number = number
-        	self.genotype = genotype
-        	self.sex = sex
-        	self.quantity = quantity
-        	self.dob = dob
-        	self.injured = injured
+      def __init__(self, number, genotype, sex, quantity=0, dob, injured):
+            self.cage_number = number
+            self.genotype = genotype
+            self.sex = sex
+            self.quantity = quantity
+            self.dob = dob
+            self.injured = injured
 
-    	def __repr__(self):
-        		return "Cage(cage_number='{self.cage_number}', " \
+      def __repr__(self):
+            return "Cage(cage_number='{self.cage_number}', " \
                   	     	"genotype='{self.genotype}', " \
                        		"sex='{self.sex}', " \
                        		"quantity={self.quantity}, " \
@@ -51,3 +53,5 @@ class Record(Base):
 		return "Record(record_id={self.record_id}, " \
                      		 "date={self.date}", \
                      		 "notes={self.notes})".format(self=self)
+
+'''
